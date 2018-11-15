308e79add9a188781b3755b23145a0de402fbdae
DZuyev <DZuyev@FGP-FRA-NB008.ftg.local>
2018-11-15 15:36:35 +0100

    "Rechnen" eingefügt und "HelloWorld" umbenannt in HausaufgabenTest

---
+++ test/helloWorld/HausaufgabenTest.java
@@ -0,0 +1,29 @@
+package helloWorld;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import uk.co.placona.helloWorld.HelloWorld;
+
+public class HausaufgabenTest {
+	public Rechnen rn;
+
+	@Test
+	public void kleineSummeTest() {
+		rn = new Rechnen();
+		String text = rn.Berechnen(3, 5);
+		assertEquals("KleineSumme", text);
+	}
+	
+	@Test
+	public void GroﬂeSummeTest() {
+		rn = new Rechnen();
+		String text = rn.Berechnen(1000, 5000);
+		assertEquals("GroﬂeSumme", text);
+	}
+	
+	
+	
+	
+}
---
+++ test/helloWorld/Rechnen.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+public class Rechnen {
+	
+	public int Berechnen(int i,int j) {
+	
+	int c = 0;
+	return 0;
+
+}
+}
